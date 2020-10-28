79e2ebdbbc70c1b7cfa8ae6933600ce573cd39e5
JOSE DANIEL DIAZ CHACON <117050320@castrocarazo.ac.cr>
2020-10-28 16:56:00 -0600

    Daniel

---
+++ Proyecto_Final/MASTER_PAGES/MaterPages.Master.designer.cs
@@ -0,0 +1,44 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace Proyecto_Final.MASTER_PAGES
+{
+
+
+    public partial class MaterPages
+    {
+
+        /// <summary>
+        /// Control head.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
+
+        /// <summary>
+        /// Control ContenidoPrincipal.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContenidoPrincipal;
+
+        /// <summary>
+        /// Control scriptspersonalizados.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder scriptspersonalizados;
+    }
+}
