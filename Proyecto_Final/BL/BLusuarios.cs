c3f1af2bba3ca2507a1066e02a82b7dcdd56bcfe
Jesús Saenz <jesussaenz04@gmail.com>
2020-10-28 21:08:06 -0600

    Clase Blusuarios/2 SP Faltantes

--- Proyecto_Final/BL/BLusuarios.cs
+++ Proyecto_Final/BL/BLusuarios.cs
@@ -12,7 +12,46 @@ namespace Proyecto_Final.BL
     {
         segurosEntities1 modeloBD = new segurosEntities1();
         
-       
+       public bool ModificaUsuario
+            (int pIdUsuario,
+             string pCedula,
+             string pGenero,
+             DateTime pFecha,
+             string pNombre,
+             string pPrimerApellido,
+             string pSegundoApellido,
+             string pDireccion,
+             string pTelefono1,
+             string pTelefono2,
+             string pCorreo,
+             string pTipoUsuario)
+        {
+            ///Crear La variable Que Se Retornara
+            int resultado = 0;
+            ///Asignar A La Variable El Resultado Del LLamado Al Procedimineto Almacenado
+            resultado = this.modeloBD.spModificaUsuario(
+                pIdUsuario, 
+                pCedula,
+                pGenero,
+                pFecha,
+                pNombre,
+                pPrimerApellido,
+                pSegundoApellido,
+                pDireccion,
+                pTelefono1,
+                pTelefono2,
+                pCorreo,
+                pTipoUsuario);
+            ///Retornar El Valor 
+            if (resultado > 0) 
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
         
     }
 }
