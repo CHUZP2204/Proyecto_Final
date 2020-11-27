8196cbf942590f30596cd2e749ab9765a16d31a1
WAGNER DANILO DIAZ DIAZ <116180080@castrocarazo.ac.cr>
2020-11-26 22:59:03 -0600

    wagner

---
+++ Proyecto_Final/BL/BLAdicciones.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Proyecto_Final.Modelo;
+
+namespace Proyecto_Final.BL
+{
+    public class BLAdicciones
+    {
+        /// <summary>
+        /// Modelo De Entity Framework
+        ///  segurosEntities1 es el modelo Base Datos
+        ///  y el Objeto modeloBD
+        /// </summary>
+        segurosEntities1 modeloBD = new segurosEntities1();
+
+    }
+}
