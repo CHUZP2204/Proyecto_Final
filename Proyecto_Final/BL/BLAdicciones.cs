a0a2a79683dd69c272d84dcd97808e194b906466
WAGNER DANILO DIAZ DIAZ <116180080@castrocarazo.ac.cr>
2020-11-26 23:09:07 -0600

    wagner

--- Proyecto_Final/BL/BLAdicciones.cs
+++ Proyecto_Final/BL/BLAdicciones.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.UI.WebControls;
+using Microsoft.Ajax.Utilities;
 using Proyecto_Final.Modelo;
 
-namespace Proyecto_Final.BL
+namespace Proyecto_Final
 {
     public class BLAdicciones
     {
