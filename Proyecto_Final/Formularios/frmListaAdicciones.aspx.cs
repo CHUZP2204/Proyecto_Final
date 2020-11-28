e1fcdff2aa954988d8348c32d968082625a4c345
WAGNER DANILO DIAZ DIAZ <116180080@castrocarazo.ac.cr>
2020-11-27 21:30:48 -0600

    se Agrega grid lista adicciones

--- Proyecto_Final/Formularios/frmListaAdicciones.aspx.cs
+++ Proyecto_Final/Formularios/frmListaAdicciones.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Proyecto_Final.Modelo;
 
 namespace Proyecto_Final.Formularios
 {
@@ -12,6 +13,38 @@ namespace Proyecto_Final.Formularios
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void btnMostrar_Click(object sender, EventArgs e)
+        {
+            this.CargaDatosGrid();
+        }
+        void CargaDatosGrid() 
+        {
+            //crear la instancia de la clase que retorna datos
+            BLAdicciones bLAdicciones = new BLAdicciones();
+            // variable contiene datos
+            List<sp_RetornaAdicciones_Result> fuenteDatos =
+                bLAdicciones.RetornaAdicciones(this.txtNombreAdiccion.Text,
+                this.txtCodigoAdiccion.Text);
+
+            //fuente datos
+            this.grdListaAdiccion.DataSource = fuenteDatos;
+
+            //muestra grid
+            this.grdListaAdiccion.DataBind();
+
+
+
+        }
+
+        protected void grdListaAdiccion_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            // indicar al grid la nueva pagina utilizando el paramentro e
+            this.grdListaAdiccion.PageIndex = e.NewPageIndex;
+
+            //cargar de nuevo el grid
+            this.CargaDatosGrid();
         }
     }
 }
